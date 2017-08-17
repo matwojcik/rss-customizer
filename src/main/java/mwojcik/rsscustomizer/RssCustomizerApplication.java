bfb8a987b4aa29c6b7c17980d637340c73bcc568
Mateusz Wojcik <mateusz.wojcik@ocado.com>
2017-08-17 15:59:36 +0200

    Initial commit of Spring Boot app

---
+++ src/main/java/mwojcik/rsscustomizer/RssCustomizerApplication.java
@@ -0,0 +1,12 @@
+package mwojcik.rsscustomizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RssCustomizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RssCustomizerApplication.class, args);
+	}
+}
